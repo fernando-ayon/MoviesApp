06468f20ee8f36ab58ebfc6ffd6a8f2ccf3b4868
Fernando Ayon <as_fernando@outlook.com>
2019-10-28 14:57:13 +0100

    Add angular material in project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+    production: true,
+    omdbapikey: '8bc9ea71',
+    omdbapi: 'http://www.omdbapi.com/?apikey='
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  omdbapikey: '8bc9ea71',
+  omdbapi: 'http://www.omdbapi.com/?apikey='
 };
 
 /*
